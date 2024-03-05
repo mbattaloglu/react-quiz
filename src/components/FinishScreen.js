2e3f1ba33f9b4de9aa13fe203c2ead655f5668a9
mbattaloglu <mmbattaloglu@gmail.com>
2024-03-05 21:20:00 +0300

    files

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,29 @@
+function FinishScreen({ dispatch, points, highscore, maxPossiblePoints }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80) emoji = "🎉";
+  if (percentage >= 50) emoji = "🙃";
+  if (percentage >= 0) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)})%
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
